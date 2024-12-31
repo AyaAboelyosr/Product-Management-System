9ac63202aa5f5a0f777e55e2c89da153b1e20e4a
Aya Mohamed <ayam80748@gmail.com>
2024-12-31 02:21:39 +0200

    Initial commit

---
+++ Product Management System/DTOs/AddProductDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Product_Management_System.DTOs
+{
+    public class AddProductDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Product Management System/Models/ProductDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Product_Management_System.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
